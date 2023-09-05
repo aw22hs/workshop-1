5640d67b5a685de283e982711666f7b180a5a730
Anja Willsund <aw22hs@student.lnu.se>
2023-09-05 11:39:18 +0200

    Latest updates

--- src/js/components/game-board/game-board.js
+++ src/js/components/game-board/game-board.js
@@ -58,31 +58,31 @@ template.innerHTML = `
 
   <div class=game-board>
     <div class="c1r1">
-      <button>1</button>
+      <button id="b1" class="gameButton">1</button>
     </div>
     <div class="c2r1">
-      <button>2</button>
+      <button id="b2" class="gameButton">2</button>
     </div>
     <div class="c3r1">
-      <button>3</button>
+      <button id="b3" class="gameButton">3</button>
     </div>
     <div class="c1r2">
-      <button>4</button>
+      <button id="b4" class="gameButton">4</button>
     </div>
     <div class="c2r2">
-      <button>5</button>
+      <button id="b5" class="gameButton">5</button>
     </div>
     <div class="c3r2">
-      <button>6</button>
+      <button id="b6" class="gameButton">6</button>
     </div>
     <div class="c1r3">
-      <button>7</button>
+      <button id="b7" class="gameButton">7</button>
     </div>
     <div class="c2r3">
-      <button>8</button>
+      <button id="b8" class="gameButton">8</button>
     </div>
     <div class="c3r3">
-      <button>9</button>
+      <button id="b9" class="gameButton">9</button>
     </div>
   </div>
 `
@@ -93,11 +93,22 @@ customElements.define('game-board',
  */
   class extends HTMLElement {
     /**
+     * Counts button clicks
+     */
+    #clickCounter = 0
+
+    /**
      * Creates an instance of the current type.
      */
     constructor () {
       super()
       this.attachShadow({ mode: 'open' })
       this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+      this.shadowRoot.querySelectorAll('button').addEventListener('click', () => {
+        this.#clickCounter++
+        console.log(this.#clickCounter)
+      })
     }
   })
+  
